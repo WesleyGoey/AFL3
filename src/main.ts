32aead825156b46f5479d5e6fc12e91a645158d1
WesleyGoey <wesley.goey@gmail.com>
2025-11-30 10:47:49 +0700

    p

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import express from "express"
 import { PORT } from "./utils/env-util"
-// import { publicRouter } from "./routes/public-api";
+import { publicRouter } from "./routes/public-api";
 // import { errorMiddleware } from "./middlewares/error-middleware";
 // import { privateRouter } from "./routes/private-api";
 
 const app = express()
 
 app.use(express.json())
-// app.use("/api", publicRouter)
+app.use("/api", publicRouter)
 // app.use("/api", privateRouter)
 // app.use(errorMiddleware)
 
